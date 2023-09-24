6d92563fffb90b76e3323a091be768966a8cedbd
dzivkovi <daniel.zivkovic@gmail.com>
2023-09-24 13:59:31 -0400

    st.cache was deprecated

--- app.py
+++ app.py
@@ -8,17 +8,17 @@ DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
             'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
-@st.cache
+@st.cache_data
 def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
+    df = pd.read_csv(DATA_URL, nrows=nrows)
     lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+    df.rename(lowercase, axis='columns', inplace=True)
+    df[DATE_COLUMN] = pd.to_datetime(df[DATE_COLUMN])
+    return df
 
 data_load_state = st.text('Loading data...')
 data = load_data(10000)
-data_load_state.text("Done! (using st.cache)")
+data_load_state.text("Done! (using st.cache_data)")
 
 if st.checkbox('Show raw data'):
     st.subheader('Raw data')
@@ -32,5 +32,5 @@ st.bar_chart(hist_values)
 hour_to_filter = st.slider('hour', 0, 23, 17)
 filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+st.subheader(f'Map of all pickups at {hour_to_filter}:00')
 st.map(filtered_data)
